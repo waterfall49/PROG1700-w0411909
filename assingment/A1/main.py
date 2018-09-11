cdabf2c754f3f0630f7a2b95b48c0c7b7f0335e6
Bae <W0411909@campus.nscc.ca>
2018-09-11 10:27:04 -0300

    commit

---
+++ assingment/A1/main.py
@@ -0,0 +1,16 @@
+"""
+Assignment Title
+
+Assignment Description
+
+Name..: HYEJUNG BAE
+ID....: w0411909
+"""
+
+_AUTHOR_ = "Student Name <w0411909@nscc.ca>"
+
+def main():
+    print("Hello World")
+
+if __name__ == "__main__":
+    main()    
