90c46ad8aa91da9d2b4fbb012fccbe8523a0bbbd
Bae <W0411909@campus.nscc.ca>
2018-09-14 11:47:08 -0300

    commit

---
+++ In Class/20180911/main.py
@@ -0,0 +1,32 @@
+"""
+Assignment Title
+
+Assignment Description
+
+Name..: HYEJUNG BAE
+ID....: w0411909
+"""
+
+_AUTHOR_ = "Student Name <w0411909@nscc.ca>"
+
+def main():
+    # input 
+    length = input("What is the lenght of walls: ")
+    width = input("What is the width of walls: ")
+    height = input("What is the height of walls: ")
+
+    # process
+    wall1 = int(length) * int(height) * 2
+    wall2 = int(width) * int(height) * 2
+    sqft = wall1 + wall2
+    # sqft2 = 2 * height ( length + width )
+    gallons = sqft / 150 
+
+    # output
+    displayGallons = str(gallons)
+    displayGallons = "You will need "+displayGallons+" gallons"
+    print(displayGallons)             
+
+
+if __name__ == "__main__":
+    main()    
