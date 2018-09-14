2f5e61b5c5de717da9f46aa2b2fdcad1cfdc7c2b
Bae <W0411909@campus.nscc.ca>
2018-09-14 12:21:22 -0300

    commit

---
+++ In Class/201809141/main.py
@@ -0,0 +1,28 @@
+"""
+Assignment Title
+
+Assignment Description
+
+Name..: HYEJUNG BAE
+ID....: w0411909
+"""
+
+_AUTHOR_ = "Student Name <w0411909@nscc.ca>"
+
+def main():
+
+    # input
+    cups = input("How many cups of coffee would you like to order: ")
+    print("You ordered "+str(cups)+" of coffee")
+
+    # process
+    preprice = 1.25 * int(cups)
+    tax = 0.15
+    price = preprice * (1 + tax)
+
+    # ouput
+    price = str(price)
+    print("The total amount of cost is $ "+price+" ")
+
+if __name__ == "__main__":
+    main()    
