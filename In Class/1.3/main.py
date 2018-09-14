bf86eef814ae5043126387b3388c63f2f156be3b
Bae <W0411909@campus.nscc.ca>
2018-09-14 12:30:32 -0300

    commit

--- In Class/1.3/main.py
+++ In Class/1.3/main.py
@@ -9,8 +9,27 @@ ID....: w0411909
 
 _AUTHOR_ = "Student Name <w0411909@nscc.ca>"
 
+import math
+
 def main():
-    print("Hello World")
+    # input 
+    length = input("What is the lenght of walls: ")
+    width = input("What is the width of walls: ")
+    height = input("What is the height of walls: ")
+
+    # process
+    wall1 = int(length) * int(height) * 2
+    wall2 = int(width) * int(height) * 2
+    sqft = wall1 + wall2
+    # sqft = 2 * height ( length + width )
+    gallons = sqft / 150 
+    gallons = math.ceil(gallons)
+
+    # output
+    displayGallons = str(gallons)
+    displayGallons = "You will need "+displayGallons+" gallons"
+    print(displayGallons)             
+
 
 if __name__ == "__main__":
     main()    
