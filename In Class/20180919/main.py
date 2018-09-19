4244b1c8ab2dff31eb9b062c6a6170b06731b41d
Bae <W0411909@campus.nscc.ca>
2018-09-19 11:26:41 -0300

    commit

---
+++ In Class/20180919/main.py
@@ -0,0 +1,19 @@
+"""
+Assignment Title
+
+Assignment Description
+
+Name..: HYEJUNG BAE
+ID....: w0411909
+"""
+
+_AUTHOR_ = "Student Name <w0411909@nscc.ca>"
+
+def main():
+    fname = "Jeff"
+    phrase = "Hello, my name is {0} "
+    phrase = phrase.format(fname)
+    print(phrase)
+
+if __name__ == "__main__":
+    main()    
