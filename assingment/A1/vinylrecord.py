7169a216daffec74392ac9a30a1c11234e095d9b
D210 User <cisco@AD.NET172.CA>
2018-09-27 13:39:39 -0300

    commit

--- assingment/A1/vinylrecord.py
+++ assingment/A1/vinylrecord.py
@@ -12,17 +12,29 @@ _AUTHOR_ = "Cathy _ HYEJUNG BAE <w0411909@nscc.ca>"
 def main():
 
     # input
-    name = input("Enter the customer's name:")
-    distance = float(input("Enter the distance in kilometers for delivery:"))
-    cost = float(input("Enter the cost of records purchased:"))
+    name1 = input("Enter the customer's name:")
+    distance = input("Enter the distance in kilometers for delivery:")
+    cost = input("Enter the cost of records purchased:")
+    name1 = str(name1)
+    distance = float(distance)
+    cost = float(cost)
 
     # process
-    deliverycost = distance * 15
-    purchsecost = cost * 1.14
+    rateperkilo = 15
+    tax = 0.14
+    deliverycost = distance * rateperkilo
+    purchsecost = cost + (cost * tax)
+    totalcost = deliverycost + purchsecost
     
     # output    
-    yearnmonth ="{0} year and {1}month(s)".format(year,month2)
-    print(yearnmonth)
+    purchasesummary = "Purchase summary for "+str(name1)+" "
+    deliverycost = "Delivery Cost:${:.2f}". format(deliverycost)
+    purchsecost = "Purchase Cost:${:.2f}". format(purchsecost)
+    totalcost = "Total Cost:${:.2f}". format(totalcost)
+    print(purchasesummary)
+    print(deliverycost)
+    print(purchsecost)
+    print(totalcost)
 
 if __name__ == "__main__":
     main()    
