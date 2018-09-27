f4bedcb573b5cc0b16598ca52b836a908f4c7644
Hyejung Bae <waterfall49@naver.com>
2018-09-28 08:49:04 +0900

    commit

--- assingment/A1/metric1.py
+++ assingment/A1/metric1.py
@@ -12,20 +12,20 @@ _AUTHOR_ = "Cathy _ HYEJUNG BAE <w0411909@nscc.ca>"
 def main():
 
     # input
-    tons = input("Enter the number of tons:")
-    stone = input("Enter the number of stone:")
-    pound = input("Enter the number of pounds:")
-    ounce = input("Enter the number of ounces:")
+    tons = int(input("Enter the number of tons:"))
+    stone = int(input("Enter the number of stone:"))
+    pound = int(input("Enter the number of pounds:"))
+    ounce = int(input("Enter the number of ounces:"))
 
     # process
     totalounce = (35840 * tons) + (224 * stone) + (16 * pound) + ounce
     totalkilos = totalounce / 35.274
     metrictons = int(totalkilos/1000)
     kilos = totalkilos % 1000
-    grams = totalounce % 35.274
+    grams = (totalounce % 35.274) / 35.274
     
     # output    
-    display ="The metric weight is {0} tons, {1} kilos, and {2:.1f} grams".format(tons,kilos,grams) 
+    display ="The metric weight is {0} tons, {1} kilos, and {2:.1f} grams". format(tons,kilos,grams) 
     print(display)
 
 if __name__ == "__main__":
