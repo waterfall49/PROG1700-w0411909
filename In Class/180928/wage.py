4344e15a213115ac6541107f72739d5334e96a68
Hyejung Bae <waterfall49@naver.com>
2018-09-29 08:50:25 +0900

    commit

--- In Class/180928/wage.py
+++ In Class/180928/wage.py
@@ -11,15 +11,17 @@ _AUTHOR_ = "Student Name <w0411909@nscc.ca>"
 
 def main():
     hour = input("Enter the the number of hours for this week(hr): ")
-    pay = input("Enter your payment per hour($): ")
+    pay = input("Enter your payment per hour($/hr): ")
     hour = float(hour)
     pay = float(pay)
     if hour <= 40 :
         wage = hour * pay
         print("Your wage is $"+str(wage)+" ")
     else :
-        wage1 =  
-    print("Have a nice trip")
+        exceedwage = (hour-40) * pay * 1.5 
+        totalwage = (40 * pay) + exceedwage   
+        print("Your wage is $"+str(totalwage)+" ")
+    print("Thank you")
 
 if __name__ == "__main__":
     main()    
