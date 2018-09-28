64d7da791c9ce2aa18cf84d2c2c7d437e27d12bd
Hyejung Bae <waterfall49@naver.com>
2018-09-28 09:08:56 +0900

    commit

---
+++ In Class/1.3/main.1.py
@@ -0,0 +1,17 @@
+"""
+Assignment Title
+
+Assignment Description
+
+Name..: HYEJUNG BAE
+ID....: w0411909
+"""
+
+_AUTHOR_ = "Student Name <w0411909@nscc.ca>"
+
+def main():
+    a = 5 % 2
+    print(a)
+
+if __name__ == "__main__":
+    main()    
