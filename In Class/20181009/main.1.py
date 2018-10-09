b9af8745e539b63578097f5ff1f276459c63ead5
Bae <W0411909@campus.nscc.ca>
2018-10-09 09:23:15 -0300

    commit

---
+++ In Class/20181009/main.1.py
@@ -0,0 +1,9 @@
+
+def main():
+    name = input("What is your name: ")
+    age = input("How old are you: ")
+    display = "My name is {0}, I am {1} years old". format(name,age)
+    print(display)
+
+if __name__ == "__main__":
+    main()    
---
+++ In Class/20181009/main.2.py
@@ -0,0 +1,5 @@
+
+def sayHi(name,age):
+    sayHi("dean",48)
+    print(f"Hello {name}, you are {age})
+
