fcf224b2e3ab64f5e4a32fc07ae4e3245407c661
Hyejung Bae <waterfall49@naver.com>
2018-10-10 23:55:36 +0900

    commit

--- In Class/20181009/main.3.py
+++ In Class/20181009/main.3.py
@@ -2,11 +2,32 @@
 def addNumber(a,b):
     return(a+b)
 
-#addNumber(199,57)
-#print(addNumber())
+def multiNumber(c,d):
+    return(c*d)
+
+def squreNumber(x):
+    return multiNumber(x,x)
+
+def printOne():
+    return("one")
+
+def printNine():
+    return("nine")
+
+def printNineOneOne():
+    print(printNine()+printOne()+printOne())
+
+print(printNineOneOne())    
+
 
 add = addNumber(199,57)
 print(add)  
+print(addNumber(199,57))
+
+a = input("a= ")
+b = input("b= ")
+add = addNumber(a,b)
+print(add)
 
 
 
---
+++ In Class/20181010/main.py
@@ -0,0 +1,48 @@
+"""
+Assignment Title
+
+Assignment Description
+
+Name..: HYEJUNG BAE
+ID....: w0411909
+"""
+
+_AUTHOR_ = "Student Name <w0411909@nscc.ca>"
+
+import math
+
+def main():
+    # input 
+    length = input("What is the lenght of walls: ")
+    width = input("What is the width of walls: ")
+    height = input("What is the height of walls: ")
+
+    # process
+
+    def multiple(a,b):
+        return(a*b)
+
+    def calceil():
+        math.ceil()    
+
+    wall1 = multiple(length,height)   
+    wall2 = multiple(width,height)
+    sqft = (wall1 + wall2) *2   
+    #wall1 = int(length) * int(height) * 2
+    #wall2 = int(width) * int(height) * 2
+    #sqft = wall1 + wall2
+    #sqft = 2 * height ( length + width )
+    gallons = sqft / 150 
+    maxgallons = calceil(gallons)
+    #gallons = math.ceil(gallons) 
+
+    # output
+    print(f"You will need {maxgallons} gallons")
+
+    #displayGallons = str(gallons)
+    #displayGallons = "You will need "+displayGallons+" gallons"
+    #print(displayGallons)             
+
+
+if __name__ == "__main__":
+    main()    
