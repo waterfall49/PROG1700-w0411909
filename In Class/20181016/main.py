ad39c8e411764b7d4637dda4657f2ab7ad10a60a
Hyejung Bae <waterfall49@naver.com>
2018-10-17 02:50:49 +0900

    commit

--- In Class/20181016/main.py
+++ In Class/20181016/main.py
@@ -3,10 +3,12 @@ import random
 num_to_guess = random.randint(1,10)
 current_guess = -1
 
-while (num_to_guess != current_guess):
-    current_guess = (input("Guess again (1-10): "))
+while num_to_guess != current_guess:
+    current_guess = input("Guess again (1-10): ")
     if not current_guess.isnumeric():
-        print("Bad :(")
+        print("Try again. Enter number!")
+    elif int(current_guess) < 1 or int(current_guess) > 10:
+        print("Try to enter the number between 1-10!")
     elif int(current_guess) > num_to_guess:
         print("That was too high")
     elif int(current_guess) < num_to_guess:
