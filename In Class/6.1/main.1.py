e800e2db27d7ab2ced717f9d5c321872e57951c5
Hyejung Bae <waterfall49@naver.com>
2018-10-23 09:59:54 +0900

    commit

--- In Class/6.1/main.1.py
+++ In Class/6.1/main.1.py
@@ -10,31 +10,27 @@ def LoanCalculator():
         Loan = input("Enter the amount of the loan($): ")
 
     InterestRate = float(input("Enter the interest rate(%): "))
-    Interestfloat = InterestRate * 0.01
+    Interestfloat = InterestRate * 0.01  # Convert % to float to calculate
     NumberOfDays = int(input("Enter number of days until it is due(days): "))
 
-    #def InterestCalculator(N_Days):
-    #    e = N_Days-1
-    #    I_Interestpay = float(Loan) * Interestfloat * ((1+Interestfloat)**e)
-    #    return(N_Days,I_Interestpay)
+    Interestpay = float(Loan) * Interestfloat  # Interestpay 
 
-    Interestpay = float(Loan) * Interestfloat
-
-    def InterestCalculator(days):
+    def InterestCalculator(days):        # Function to calculate accumulate interestpay
         additionalpay = Interestpay * Interestfloat
-        Interestpay = Interestpay + additionalpay
-        return(days,Interestpay)     
+        I_Interestpay = Interestpay + additionalpay
+        return(days,I_Interestpay)     
 
     i=1
     while i<=NumberOfDays:
-        if i == 1 : 
-            Interestpay = Interestpay
-            print(f"{i} - $  {Interestpay:.2f}  |" +  "=" * math.ceil(Interestpay) )
+        if i == 1: 
+            print(f"\n{i} - $  {Interestpay:.2f}  |" +  "=" * math.ceil(Interestpay) )
+            Totalpay = Interestpay + float(Loan)   # Totalpay is includng loan money
         else:
             Day,Interestpay=InterestCalculator(i)
             print(f"{Day} - $  {Interestpay:.2f}  |" +  "=" * math.ceil(Interestpay) )
+            Totalpay = Totalpay + Interestpay  # Totalpay is accumulated 
         i = i+1
 
-    #print(f"Total Owing : ${Totalpay:.2f} ")
+    print(f"\nTotal Owing : ${Totalpay:.2f} ")
 
 LoanCalculator()
