a57b3e87ab4203e98baca042c9d588f5e68c208e
Hyejung Bae <waterfall49@naver.com>
2018-10-27 00:07:02 +0900

    commit

---
+++ In Class/20181026/enumerate.py
@@ -0,0 +1,17 @@
+
+def main():
+
+    hours = [8,2,13,1,4,7]
+
+    # counter = 1 
+    # for h in hours : 
+    #     print(h)
+    #     print(counter)
+    #     counter = counter + 1 
+
+    c = 1
+    for c, h in enumerate(hours) : 
+        print(h)
+        print(c+1)    
+
+main()
--- In Class/20181026/hyejungBAE.py
+++ In Class/20181026/hyejungBAE.py
@@ -1,19 +1,33 @@
 
 def main():
 
-    # input
     num_list = [  ]
 
-    i=0
-    while i=0 :
-        num = input("Enter your number: ")
-        num_list.append(num)
-        print(list(num_list))    
- 
+    i = 0
+    while i == 0 :
+        answer = input("Do you want to continue adding to the list?(yes or no): ")
+        answer = answer.lower()
+        if answer == "yes" :
+            n = 0
+            while n == 0 : 
+                maybe_num = input("Enter your number: ")
+                if not maybe_num.isnumeric(): 
+                    print("That is not a numeric! ")
+                else : 
+                    num = int(maybe_num)
+                    num_list.append(num)
+                    print(num_list)
+                    n = n + 1
+        else :
+            print("Thank you")
+            i = i + 1
 
-    # process
- 
+    length = len(num_list)
+    total = sum(num_list)  
+    average = total / length      
 
-    # output    
+    print("The length of number is: " + str(length))    
+    print("The sum of numbers is: " +str(total))
+    print("The average of numbers is: " +str(average))
 
 main()
