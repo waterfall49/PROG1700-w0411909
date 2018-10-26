1067c66b3ba3f36a774677fd4a644564b65818c8
Bae <W0411909@campus.nscc.ca>
2018-10-26 10:43:32 -0300

    commit

---
+++ In Class/20181026/hyejungBAE.py
@@ -0,0 +1,19 @@
+
+def main():
+
+    # input
+    num_list = [  ]
+
+    i=0
+    while i=0 :
+        num = input("Enter your number: ")
+        num_list.append(num)
+        print(list(num_list))    
+ 
+
+    # process
+ 
+
+    # output    
+
+main()
