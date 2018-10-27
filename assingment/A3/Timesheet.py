dccbe8c1eb57e3319f0c9f1ff169ecea44987c6d
Hyejung Bae <waterfall49@naver.com>
2018-10-28 00:11:07 +0900

    commit

---
+++ assingment/A3/Timesheet.py
@@ -0,0 +1,13 @@
+
+def main():
+
+    day_hours = []
+
+    i=1
+    while i <= 5 :
+        hours = input(f"Enter hours worked on Day #{i}: ") 
+        day_hours.append(hours)
+        i = i + 1
+
+
+main()
