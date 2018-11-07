034c3f485a229c0731aeff29ff3b1c6689da5020
Bae <W0411909@campus.nscc.ca>
2018-11-07 12:18:48 -0400

    commit

--- In Class/20181107/test.py
+++ In Class/20181107/test.py
@@ -1,9 +1,22 @@
 inp_obj = open("test.txt")
 out_obj = open("outfile.txt","w")
+
+print(inp_obj.read())
+#print(inp_obj.tell())
+# inp_obj.seek(2)
+print(inp_obj.read(2))
+
+
+
+# strContent = inp_obj.readline()
+# print(strContent)
+
 for line_str in inp_obj:
     out_obj.write(line_str)
-a = out_obj.seek(1)
-print(a)
+print(inp_obj.read())
+#a = inp_obj.seek(1)
+#print(a)
+
 # out_obj = open("outfile.txt")
 #print(out_obj.read())
 
