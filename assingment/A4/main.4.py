4f28663071d3f24ee235241f579912c99af346b1
Mark Farrell <markfarr2011@gmail.com>
2018-11-13 12:12:57 -0400

    commit

--- assingment/A4/main.4.py
+++ assingment/A4/main.4.py
@@ -1,26 +1,41 @@
 
+import math
+
 objFile = open("ateam_Original.txt")
-objFile.seek(4)
-strContent = objFile.readline()
-print(strContent)
-# outFile = open("outFile.txt","w")
+outFile = open("outFile.txt","w")
 
-# def strMSG(L_Line,L_Num):
-#     strMsg = "Length("
-#     strMsg += str(L_Num)
-#     strMsg += ") : "
-#     strMsg += L_Line
-#     return(strMsg)
+def strMSG(L_Line,L_Num):
+    strMsg = "Length("
+    strMsg += str(L_Num)
+    strMsg += ") : "
+    strMsg += L_Line
+    return(strMsg)
 
-# line_number = 1
-# for line in objFile : 
-#     # print(line_number)
-#     line = line.replace("\n","") 
-#     line = line.upper()
-#     display = strMSG(line,line_number)
-#     display += "\n"
-#     outFile.write(display)
-#     line_number += 1 
+def omitline(File,Num):
+    randomNum = random.randint(1,Num)
+    c = 1
+    for l in File : 
+        if c == randomNum : 
+            l = l.remove()
+        else :
+            l = l 
+        c += 1 
+    print(l)        
 
-# objFile.close()
-# outFile.close()
+def main():
+    line_number = 1
+    for line in objFile : 
+        line = line.replace("\n","") 
+        if len(line) > 20 :
+            line = line.lower()
+        else : 
+            line = line.upper()
+        display = strMSG(line,line_number)
+        display += "\n"
+        outFile.write(display)
+        line_number += 1 
+
+    objFile.close()
+    outFile.close()
+
+main()
