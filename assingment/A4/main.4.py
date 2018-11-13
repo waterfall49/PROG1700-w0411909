fa3ac203b7b9065760e2771e1417b32ba28919d5
Bae <W0411909@campus.nscc.ca>
2018-11-13 10:02:31 -0400

    commit

--- assingment/A4/main.4.py
+++ assingment/A4/main.4.py
@@ -1,4 +1,25 @@
 
 objFile = open("ateam_Original.txt")
-strContent = objFile.read()
-print(strContent)
+# strContent = objFile.read()
+# print(strContent)
+outFile = open("outFile.txt","w")
+
+def strMSG(L_Line,L_Num):
+    strMsg = "Length("
+    strMsg += str(L_Num)
+    strMsg += ") : "
+    strMsg += L_Line
+    return(strMsg)
+
+line_number = 1
+for line in objFile : 
+    # print(line_number)
+    line = line.replace("\n","") 
+    line = line.upper()
+    display = strMSG(line,line_number)
+    display += "\n"
+    outFile.write(display)
+    line_number += 1 
+
+objFile.close()
+outFile.close()
