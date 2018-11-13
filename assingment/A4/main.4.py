17bbf85f4eafe26a879748e9ad1ce70dd032714b
Bae <W0411909@campus.nscc.ca>
2018-11-13 10:23:18 -0400

    commit

--- assingment/A4/main.4.py
+++ assingment/A4/main.4.py
@@ -1,25 +1,26 @@
 
 objFile = open("ateam_Original.txt")
-# strContent = objFile.read()
-# print(strContent)
-outFile = open("outFile.txt","w")
+objFile.seek(4)
+strContent = objFile.readline()
+print(strContent)
+# outFile = open("outFile.txt","w")
 
-def strMSG(L_Line,L_Num):
-    strMsg = "Length("
-    strMsg += str(L_Num)
-    strMsg += ") : "
-    strMsg += L_Line
-    return(strMsg)
+# def strMSG(L_Line,L_Num):
+#     strMsg = "Length("
+#     strMsg += str(L_Num)
+#     strMsg += ") : "
+#     strMsg += L_Line
+#     return(strMsg)
 
-line_number = 1
-for line in objFile : 
-    # print(line_number)
-    line = line.replace("\n","") 
-    line = line.upper()
-    display = strMSG(line,line_number)
-    display += "\n"
-    outFile.write(display)
-    line_number += 1 
+# line_number = 1
+# for line in objFile : 
+#     # print(line_number)
+#     line = line.replace("\n","") 
+#     line = line.upper()
+#     display = strMSG(line,line_number)
+#     display += "\n"
+#     outFile.write(display)
+#     line_number += 1 
 
-objFile.close()
-outFile.close()
+# objFile.close()
+# outFile.close()
