575c83f3309cbedd26f15fcea2754fb7fcbcd18f
Bae <W0411909@campus.nscc.ca>
2018-11-14 11:18:45 -0400

    commit

--- In Class/20181114/main.1.py
+++ In Class/20181114/main.1.py
@@ -1,14 +1,14 @@
-choices = ["chicken","iceberg","romantic i"]
-strtemplete = "The _1_ jump over the _2_ to get to their _3_"
+
+choices = []
+openfile = open("template.txt")
+strtemplete = openfile.read()
+
+for i in range(0,3):
+    answer = input(f"Enter the choice{i+1} : ")
+    choices.append(answer)
 
 strtemplete = strtemplete.replace("_1_", choices[0])
 strtemplete = strtemplete.replace("_2_", choices[1])
 strtemplete = strtemplete.replace("_3_", choices[2])
-
-for i in range(0,3):
-    strPH = "_"
-    strPH += str(i+1)
-    strPH = "_"
-    strtemplete = strtemplete.replace(strPH, choices[i])
 
 print(strtemplete)
