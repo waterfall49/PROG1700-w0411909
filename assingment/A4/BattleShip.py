74668ad2883ed8150c8694b63c1931cb739d6f5d
Hyejung Bae <waterfall49@naver.com>
2018-11-16 08:03:19 +0900

    commit

--- assingment/A4/BattleShip.py
+++ assingment/A4/BattleShip.py
@@ -1,12 +1,23 @@
 
 def main():
 
-    table = [ [ 0 for i in range(6) ] for j in range(6) ]
-    print(table)
-    for d1 in range(6):
-        for d2 in range(6):
-            table[d1][d2]= d1+d2+2
-    print(table)
+    objFile = open("map.txt")
+
+    # print(objFile.read())
+
+    # table = [ [ 0 for i in range(6) ] for j in range(6) ]
+
+    row_list = []
+    # column_list = []
+    for row in objFile:
+        row = row.replace("\n","")
+        row_list.append(row)
+        # c_list = row.split(",")
+        # c_list = int(c_list)
+        # column_list.append(c_list)
+
+    # print(column_list)
+    print(row_list)
 
 if __name__ == "__main__":
     main()    
