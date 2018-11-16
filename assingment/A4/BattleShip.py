a332e68ef27269d9e2c114bfdd23cc079870914e
Hyejung Bae <waterfall49@naver.com>
2018-11-16 23:35:36 +0900

    commit

--- assingment/A4/BattleShip.py
+++ assingment/A4/BattleShip.py
@@ -8,13 +8,25 @@ def main():
         line = line
     print(line)
 
-    Null_Array = [ [ " " for i in range(0,10) ] for j in range(0,10) ]
+    dict = { i :[chr(32) for i in range(1,11)] for i in range(1,11)}
+    print(dict)
 
-    i = 1
-    for row in Null_Array:
-        # row = row.replace("\n","")
-        print(f"{i:2d} {row}")
-        i += 1 
+
+    
+    # for L_line in F_File : 
+    # L_line = L_line.replace("\n","")
+    # listLine = L_line.split(",")
+    # W_Type = listLine[0]
+    # W_Choice = listLine[1:]
+    # D_Lines[W_Type] = W_Choice
+
+    # Null_Array = [ [ " " for i in range(0,10) ] for j in range(0,10) ]
+
+    # i = 1
+    # for row in Null_Array:
+    #     row = row.replace("\n","")
+    #     print(f"{i:2d} {row}")
+    #     i += 1 
 
     # i = 1
     # for row in Null_Array:
@@ -24,7 +36,7 @@ def main():
 
     # for r in range(1,11):
     #     print(f"{r:2d}")
-    
+
 
     objFile = open("map.txt")
 
