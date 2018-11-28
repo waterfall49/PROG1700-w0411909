c761be4b16741a70ebe98e302dc820e6eb8a6d8c
Hyejung Bae <waterfall49@naver.com>
2018-11-28 12:15:39 +0900

    commit

---
+++ GUI/Ui_helloworld.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'c:\Users\water\AppData\Local\Programs\Python\Python37-32\Scripts\helloworld.ui'
+#
+# Created by: PyQt5 UI code generator 5.11.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(510, 337)
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setWeight(75)
+        MainWindow.setFont(font)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButtonHello = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButtonHello.setGeometry(QtCore.QRect(70, 140, 191, 81))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        font.setBold(True)
+        font.setUnderline(True)
+        font.setWeight(75)
+        self.pushButtonHello.setFont(font)
+        self.pushButtonHello.setObjectName("pushButtonHello")
+        self.labelMessage = QtWidgets.QLabel(self.centralwidget)
+        self.labelMessage.setGeometry(QtCore.QRect(10, 70, 91, 21))
+        self.labelMessage.setObjectName("labelMessage")
+        self.lineEditMessage = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEditMessage.setGeometry(QtCore.QRect(110, 60, 201, 41))
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        self.lineEditMessage.setFont(font)
+        self.lineEditMessage.setObjectName("lineEditMessage")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 510, 34))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButtonHello.setText(_translate("MainWindow", "Say Hello"))
+        self.labelMessage.setText(_translate("MainWindow", "Message:"))
+
---
+++ GUI/changed_file.pyw
@@ -0,0 +1,51 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
+
+#ADD IMPORT STATEMENT FOR YOUR GENERATED UI.PY FILE HERE
+import Ui_helloworld
+#      ^^^^^^^^^^^ Change this!
+
+#CHANGE THE SECOND PARAMETER (Ui_ChangeMe) TO MATCH YOUR GENERATED UI.PY FILE
+class MyForm(QMainWindow, Ui_helloworld.Ui_MainWindow):
+#                         ^^^^^^^^^^^   Change this!
+
+    # DO NOT MODIFY THIS CODE
+    def __init__(self, parent=None):
+        super(MyForm, self).__init__(parent)
+        self.setupUi(self)
+    # END DO NOT MODIFY
+
+        # ADD SLOTS HERE, indented to this level (ie. inside def __init__)
+        self.pushButtonHello.clicked.connect(self.hellobutton_clicked)
+
+    # ADD SLOT FUNCTIONS HERE
+    # These are the functions your slots will point to
+    # Indent to this level (ie. inside the class, at same level as def __init__)
+    def hellobutton_clicked(self):
+        # Call the SetMessage() function
+        self.SetMessage()
+
+#Example Slot Function
+#   def SaveButton_Clicked(self):
+#       Make a call to the Save() helper function here
+
+    #ADD HELPER FUNCTIONS HERE
+    # These are the functions the slot functions will call, to 
+    # contain the custom code that you'll write to make your progam work.
+    # Indent to this level (ie. inside the class, at same level as def __init__)
+    def SetMessage(self):
+        # Set Hello message in line edit box
+        self.lineEditMessage.setText("Hello World")
+
+#Example Helper Function
+#    def Save(self):
+#       Implement the save functionality here
+
+# DO NOT MODIFY THIS CODE
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    the_form = MyForm()
+    the_form.show()
+    sys.exit(app.exec_())
+# END DO NOT MODIFY
